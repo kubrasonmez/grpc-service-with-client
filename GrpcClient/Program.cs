f76e4ccca06ceb7e1226fef0ccd5dab9f653cdb2
Kübra Sönmez <ksonmez@cornea.aero>
2020-11-18 08:16:10 +0300

    Implemented simple grpc service and client.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var client = new GrpcService.Greeter.GreeterClient(channel);
+
+            var request = new GrpcService.HelloRequest()
+            {
+                Name = "kubra"
+            };
+
+            var response = await client.SayHelloAsync(request);
+
+            Console.WriteLine(response.Message);
+        }
+    }
+}
